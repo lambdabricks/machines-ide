b9c1398629d756030b1b9fb7c9d6e362ad1c3292
Daniel Garcia <pel_daniel@hotmail.com>
2015-09-06 04:16:35 -0500

    Pipe as svg path

---
+++ first_pipe/app.js
@@ -0,0 +1,51 @@
+var s = Snap("#svgout");
+var g_delta = 50;
+var g_bezier_delta = g_delta * 3;
+
+// var path = "M50,50 C50,200 300,100 300,250";
+// s.path(path).addClass("line");
+// s.path("M100,0 C100,150 350,50 350,200").addClass("line");
+
+sidePipe(s, 50, 50, 350, 200);
+
+function verticalPipe(snap, x, y, size) {
+  var path = "M400,50h50v150h-50Z";
+
+  snap.path(path).addClass("line");
+}
+
+function sidePipe(snap, x1, y1, x2, y2) {
+  var x_delta = x2 - x1;
+  var y_delta = y2 - y1;
+
+  // first curve
+  var path = left_bezier_path(x1, y1, x2, y2) + right_bezier_path(x1, y1, x2, y2);
+  snap.path(path).addClass("line");
+}
+
+function left_bezier_path(x1, y1, x2, y2) {
+  var delta = g_delta;
+  var bezier_delta = g_bezier_delta;
+  var x_delta = x2 - x1;
+  var y_delta = y2 - y1;
+
+  var path = "M" + x1 + "," + y1 + "v" + delta;
+  path += "c0," + bezier_delta + " " + x_delta + ",0 " + x_delta + "," + y_delta;
+  path += "h" + delta;
+
+  return path;
+}
+
+function right_bezier_path(x1, y1, x2, y2) {
+  var delta = -g_delta;
+  var bezier_delta = -g_bezier_delta;
+  var x_delta = x1 - x2;
+  var y_delta = y1 - y2;
+
+  // var path = "M" + x1 + "," + x2;
+  var path = "v" + delta;
+  path += "c0," + bezier_delta + " " + x_delta + ",0 " + x_delta + "," + y_delta;
+  path += "z";
+
+  return path;
+}
