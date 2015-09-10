f5c68165bfb57db1e1faf66680d1b81cb7d0f35f
Daniel Garcia <pel_daniel@hotmail.com>
2015-09-10 04:42:37 -0500

    Fix drawing of pipe node on named functions

--- first_prototype_ide/ide.js
+++ first_prototype_ide/ide.js
@@ -10,13 +10,13 @@ var Point = function(x, y) {
 };
 
 var FunctionNode = function(x, y, total_inputs, total_outputs, name) {
-    this.pipe_width           = pipe_width;
-    this.function_node_height = this.node_height();
-    this.function_node_width  = this.node_width();
-    this.origin               = new Point(x, y);
     this.name                 = name;
+    this.pipe_width           = pipe_width;
+    this.origin               = new Point(x, y);
     this.total_inputs         = total_inputs;
     this.total_outputs        = total_outputs;
+    this.function_node_height = this.node_height();
+    this.function_node_width  = this.node_width();
 };
 
 FunctionNode.prototype.anonymous = function() {
@@ -24,11 +24,11 @@ FunctionNode.prototype.anonymous = function() {
 };
 
 FunctionNode.prototype.node_height = function() {
-  return this.anonymous ? anonymous_function_node_height : function_node_height;
+  return this.anonymous() ? anonymous_function_node_height : function_node_height;
 };
 
 FunctionNode.prototype.node_width = function() {
-  if(this.anonymous) {
+  if(this.anonymous()) {
     return this.pipe_width * 13;
   } else {
     return this.pipe_width * (this.total_inputs * 2 + 1);
@@ -43,7 +43,7 @@ FunctionNode.prototype.inputPoint = function(index) {
   var multiplier = 2;
   var offset     = 1;
 
-  if(this.anonymous) {
+  if(this.anonymous()) {
     multiplier = 4;
     offset     = 0;
     index++;
@@ -53,15 +53,15 @@ FunctionNode.prototype.inputPoint = function(index) {
 };
 
 FunctionNode.prototype.nodeBodyClass = function() {
-  return this.anonymous ? "main-body" : "body";
+  return this.anonymous() ? "main-body" : "body";
 }
 
 FunctionNode.prototype.nodeInputClass = function() {
-  return this.anonymous ? "main input" : "input";
+  return this.anonymous() ? "main input" : "input";
 }
 
 FunctionNode.prototype.nodeOutputClass = function() {
-  return this.anonymous ? "main output" : "output";
+  return this.anonymous() ? "main output" : "output";
 }
 
 FunctionNode.prototype.draw = function() {
@@ -74,52 +74,24 @@ FunctionNode.prototype.draw = function() {
 
   for(var i = 0; i < this.total_inputs; i++) {
     var input = snap.rect(this.inputPoint(i), this.origin.y, this.pipe_width, this.pipe_width).addClass(this.nodeInputClass());
-    input.click(function() {
-      if(selected_input && selected_input != this) {
-        selected_input.removeClass('selected');
-      }
-
-      if(selected_output) {
-        new PipeNode(this, selected_output).draw();
-        selected_output.removeClass('selected');
-        selected_output = undefined;
-        selected_input = undefined;
-      } else {
-        this.toggleClass('selected');
-        selected_input = this.hasClass('selected') ? this : undefined;
-      }
-    });
+    input.click(this.anonymous() ? inputClickEvent : outputClickEvent);
 
     inputs.push(input);
     group.add(input);
   }
 
   var output = snap.rect(this.outputPoint(), this.origin.y + this.pipe_width + this.function_node_height, this.pipe_width, this.pipe_width).addClass(this.nodeOutputClass());
-  output.click(function() {
-    if(selected_output && selected_output != this) {
-      selected_output.removeClass('selected');
-    }
-
-    if(selected_input) {
-      new PipeNode(selected_input, this).draw();
-      selected_input.removeClass('selected');
-      selected_input = undefined;
-      selected_output = undefined;
-    } else {
-      this.toggleClass('selected');
-      selected_output = this.hasClass('selected') ? this : undefined;
-    }
-  });
+  output.click(this.anonymous() ? outputClickEvent : inputClickEvent);
 
   outputs.push(output);
   group.add(output);
 
-  if(! this.anonymous) {
+  if(! this.anonymous()) {
     var outline = snap.path(this.functionNodeOutlinePath()).addClass("function-outline");
     group.add(outline);
   }
 
-  if(! this.anonymous) {
+  if(! this.anonymous()) {
     var text = snap.text(this.origin.x + (this.pipe_width / 2), this.origin.y + this.function_node_height + this.pipe_width - 10, this.name).addClass("function_name");
     group.add(text);
   }
@@ -184,8 +156,42 @@ PipeNode.prototype.draw = function() {
 };
 
 function absolutePosition(element) {
+  // clientRect = element.node.getBoundingClientRect();
   globalMatrix = element.transform().globalMatrix;
   bbox = element.getBBox();
 
   return new Point(globalMatrix.x(bbox.x, bbox.y), globalMatrix.y(bbox.x, bbox.y));
+  // return new Point(clientRect.left, clientRect.top);
+}
+
+function inputClickEvent() {
+  if(selected_input && selected_input != this) {
+    selected_input.removeClass('selected');
+  }
+
+  if(selected_output) {
+    new PipeNode(this, selected_output).draw();
+    selected_output.removeClass('selected');
+    selected_output = undefined;
+    selected_input = undefined;
+  } else {
+    this.toggleClass('selected');
+    selected_input = this.hasClass('selected') ? this : undefined;
+  }
+}
+
+function outputClickEvent() {
+  if(selected_output && selected_output != this) {
+    selected_output.removeClass('selected');
+  }
+
+  if(selected_input) {
+    new PipeNode(selected_input, this).draw();
+    selected_input.removeClass('selected');
+    selected_input = undefined;
+    selected_output = undefined;
+  } else {
+    this.toggleClass('selected');
+    selected_output = this.hasClass('selected') ? this : undefined;
+  }
 }
