e46d7bcd4ae578e735968d9514f8255b45526af6
Daniel Garcia <pel_daniel@hotmail.com>
2015-09-10 04:41:39 -0500

    Click on named functions adds it to the workspace

--- first_prototype_ide/ui.js
+++ first_prototype_ide/ui.js
@@ -1,4 +1,17 @@
+var function_signatures = {
+  "&&": { name: "&&", inputs: 2, outputs: 1 },
+  "||": { name: "||", inputs: 2, outputs: 1 },
+  "!":  { name: "!",  inputs: 1, outputs: 1 }
+};
+
 $('#createFunctionButton').click(function() {
   $('#createFunctionModal').modal('hide');
   new FunctionNode(100, 50, + $('#total_inputs').val(), + $('#total_outputs').val()).draw();
+});
+
+$('.panel-group').on('click', '.add-function', function() {
+  var function_name = $(this).data('name');
+  var function_signature = function_signatures[function_name];
+
+  new FunctionNode(50, 50, function_signature.inputs, function_signature.outputs, function_signature.name).draw();
 });
